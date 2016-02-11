d9b70863d1c6aad394e9e1a7594251a9c2d70214
Viacheslav Kabanovich <scabanovich@exadel.com>
2016-02-11 12:37:42 -0800

    Feb 11 2016

---
+++ SmallPuzzles/src/olia/LastLittleBlack.java
@@ -0,0 +1,45 @@
+package olia;
+
+public class LastLittleBlack {
+	int[][] table;
+
+	public void computeTable(int maxN, int maxM) {
+		table = new int[maxN + 1][maxM + 1];
+		for (int m = 1; m <= maxM; m++) {
+			table[1][m] = 1;
+			for (int n = 2; n <= maxN; n++) {
+				int f = (m <= n || (m % n) == 0) ? m : m % n;
+				int l = f + table[n - 1][m];
+				while(l > n) l -= n;
+				table[n][m] = l;
+			}
+		}
+	}
+
+	public void print() {
+		for (int m = 1; m < table[0].length; m++) {
+			String ms = "" + m;
+			if(ms.length() < 2) ms = " " + ms;
+			System.out.print("m=" + ms + " : ");
+			for (int n = 1; n < table.length; n++) {
+				String s = "" + table[n][m];
+				while(s.length() < 3) s = " " + s;
+				System.out.print(" " + s);
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		for (int m = 1; m < table.length; m++) {
+			String s = "" + table[m][m];
+			while(s.length() < 3) s = " " + s;
+			System.out.print(" " + s);
+		}
+	}
+
+	public static void main(String[] args) {
+		LastLittleBlack p = new LastLittleBlack();
+		p.computeTable(20, 20);
+		p.print();
+	}
+
+}
