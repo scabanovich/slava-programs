d94ff52315fb92c1844ea8931998104d99c9f827
scabanovich <slkabanovich@gmail.com>
2020-08-08 23:16:48 -0700

    Added content to code generator

---
+++ euler/src/main/webapp/resources/codegen/codegen.js
@@ -0,0 +1,69 @@
+var cg = {};
+
+cg.templates = new Map();
+cg.selected = null;
+cg.autocopy = false;
+      
+cg.loadTemplates = function() {
+	var e = document.getElementById("templates");
+	var c = e.childNodes;
+	for (var i = 0; i < c.length; i++) {
+		if (c[i] instanceof Element) {
+			var id = c[i].getAttribute("id");
+				cg.templates.set(id, c[i]);    			  
+			}
+	}
+	e.parentElement.removeChild(e);
+}
+
+cg.setTemplate = function(templateId) {
+	var t = cg.templates.get(templateId);
+	cg.selected = t;
+	if (t) {
+		var e = document.getElementById("formArea");
+		e.innerHTML = "";
+		e.appendChild(t);
+		cg.generate();
+	}
+}
+
+cg.generate = function() {
+	if (cg.selected) {
+		var ev = document.createEvent('Events')
+		ev.initEvent("click", true, false);
+		cg.selected.dispatchEvent(ev);
+		if (cg.autocopy) {
+			setTimeout(function() { cg.copyToClipboard('output'); }, 100);
+		}
+	}
+}
+  
+cg.copyToClipboard = function(sourceId) {
+	var active = document.activeElement;
+	var e = document.getElementById(sourceId);
+	e.select();
+	e.setSelectionRange(0, e.value.length);
+	document.execCommand("copy");
+	e.setSelectionRange(e.value.length, e.value.length);
+	active.focus();
+}
+   
+cg.writeToOut = function(text) {
+	var e = document.getElementById("output");
+	var ls = text.split("\n");
+	var indent = document.getElementById("indent").value;
+	var is = (indent == "1") ? "    " 
+		   : (indent == "2") ? "        " 
+		   : (indent == "3") ? "            " 
+		   : "";
+	var indentedText = "";
+	for (var i = 0; i < ls.length; i++) {
+		indentedText += is + ls[i] + "\n";
+	}
+	e.value = indentedText;
+}
+      
+cg.value = function(inputId) {
+	return document.getElementById(inputId).value;
+}
+      
